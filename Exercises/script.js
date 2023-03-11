03e22c5b5e3e936d9961213aa6916cf6e999e4c7
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-03-10 19:08:30 -0700

    adding mod4_solution

---
+++ Exercises/script.js
@@ -0,0 +1,121 @@
+// function orderChicker(sideDish) {
+//     sideDish = sideDish || "whatever"; 
+//     console.log("chicken with " + sideDish)
+// }
+
+// orderChicker("noodles"); 
+// orderChicker(); 
+
+
+// var x = 10;
+// if ( (null) || (console.log("Hello1")) || x > 5 ) {
+//   console.log("Hello");
+// }
+
+
+// // Objection is a collection of
+// // name and values pairs
+
+// // two types
+
+// // var company = new Object(); 
+// // company.name  = "SandyHills"
+// // company.ceo = new Object(); 
+// // company.ceo.lastName = "Dukuzeyesu"
+// // company.ceo.color = "Blue"
+
+// // company.years = 235; 
+// // console.log("company first Name " + 
+// // company.ceo.lastName); 
+
+// var company = {
+//     name: "facebook", 
+//     ceo: {
+//         name : "Gloria",
+//         color : "Blue"
+//     },
+//     years : 2445,
+//     "location":  "Kigali"
+// }; 
+
+// // var ceo = company["ceo"]; 
+// console.log(company["ceo"]["name"]); 
+
+
+// function makeMultiplier(multiplier) {
+//     var myfun = function(x) {
+//         return multiplier * x; 
+//     }
+
+//     return myfun; 
+// }
+
+// var multiplyBy3 = makeMultiplier(3) ;
+
+// console.log( "multiplyBy3 "); 
+
+// console.log(multiplyBy3(10)); 
+
+// var array = new Array() 
+// array[0] = "Gloria"; 
+// array[1] = 2; 
+// array[2] = function (name) {
+// console.log("my name is " + name); 
+// }
+
+// array[3] = {course: "Html, Css, and Js"}; 
+
+// console.log(array); 
+
+// array[2] (array[0]) ; 
+// console.log(array[3].course); 
+
+
+// // short hand array creation 
+//  var names = [
+//     {name: "gogo"}, 
+//     1, 
+//     {name: "joe"}
+// ]; 
+
+// var Names = ["ttt", "tt" ,"yiy"]; 
+
+// Names [100] = "Gloria"; 
+
+// for(var i = 0; i < Names.length; i++) {
+//     console.log(Names[i]); 
+// }
+
+
+
+ //console.log(names[2].name);
+
+
+ var Name2 = ["ttt", "tt" ,"yiy"]; 
+
+ var myObj = {
+    name: "Gloria", 
+    course: "HTML", 
+    platform:"coursera"
+ }; 
+
+//  for(var prop in myObj) {
+//     console.log( prop + ": " + myObj[prop]);
+//  }
+
+
+ Name2.greetings = "hi"; 
+ for(var name in Name2) {
+    console.log("Hello " + Name2[name])
+ }
+
+ console.log(Name2); 
+
+
+ // js closures
+
+ 
+
+
+
+
