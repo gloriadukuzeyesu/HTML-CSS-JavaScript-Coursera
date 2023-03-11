0329bef1e51a78435371c2fe3b7ba2941aaa94e5
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-03-11 02:24:06 -0700

    new exercises

--- Exercises/script.js
+++ Exercises/script.js
@@ -1,121 +1,66 @@
-// function orderChicker(sideDish) {
-//     sideDish = sideDish || "whatever"; 
-//     console.log("chicken with " + sideDish)
-// }
+// DOM Manipulation
 
-// orderChicker("noodles"); 
-// orderChicker(); 
+document.addEventListener("DOMContentLoaded",
+   function (event) {
 
+      function sayHello(event) {
 
-// var x = 10;
-// if ( (null) || (console.log("Hello1")) || x > 5 ) {
-//   console.log("Hello");
-// }
+         console.log(event);
 
+         this.textContent = "Said it!"
+         var name = document.getElementById("name").value;
+         var messsage = "<h2>Hello " + name + "!</h2>";
+         // document.getElementById("content").textContent = messsage; 
+         document.getElementById("content").innerHTML = messsage;
 
-// // Objection is a collection of
-// // name and values pairs
+         if (name == "student") {
+            var title =
+               document
+                  .querySelector("h1")
+                  .textContent;
+            title += " & Loving it! ";
 
-// // two types
+            document
+               .querySelector("h1")
+               .textContent = title;
+         }
 
-// // var company = new Object(); 
-// // company.name  = "SandyHills"
-// // company.ceo = new Object(); 
-// // company.ceo.lastName = "Dukuzeyesu"
-// // company.ceo.color = "Blue"
-
-// // company.years = 235; 
-// // console.log("company first Name " + 
-// // company.ceo.lastName); 
-
-// var company = {
-//     name: "facebook", 
-//     ceo: {
-//         name : "Gloria",
-//         color : "Blue"
-//     },
-//     years : 2445,
-//     "location":  "Kigali"
-// }; 
-
-// // var ceo = company["ceo"]; 
-// console.log(company["ceo"]["name"]); 
+         // event handles are functions that you handle 
+         // in a specific cycle
+      }
+      document.querySelector("button")
+         .addEventListener("click", sayHello);
 
 
-// function makeMultiplier(multiplier) {
-//     var myfun = function(x) {
-//         return multiplier * x; 
-//     }
-
-//     return myfun; 
-// }
-
-// var multiplyBy3 = makeMultiplier(3) ;
-
-// console.log( "multiplyBy3 "); 
-
-// console.log(multiplyBy3(10)); 
-
-// var array = new Array() 
-// array[0] = "Gloria"; 
-// array[1] = 2; 
-// array[2] = function (name) {
-// console.log("my name is " + name); 
-// }
-
-// array[3] = {course: "Html, Css, and Js"}; 
-
-// console.log(array); 
-
-// array[2] (array[0]) ; 
-// console.log(array[3].course); 
-
-
-// // short hand array creation 
-//  var names = [
-//     {name: "gogo"}, 
-//     1, 
-//     {name: "joe"}
-// ]; 
-
-// var Names = ["ttt", "tt" ,"yiy"]; 
-
-// Names [100] = "Gloria"; 
-
-// for(var i = 0; i < Names.length; i++) {
-//     console.log(Names[i]); 
-// }
-
-
-
- //console.log(names[2].name);
-
-
- var Name2 = ["ttt", "tt" ,"yiy"]; 
-
- var myObj = {
-    name: "Gloria", 
-    course: "HTML", 
-    platform:"coursera"
- }; 
-
-//  for(var prop in myObj) {
-//     console.log( prop + ": " + myObj[prop]);
-//  }
-
-
- Name2.greetings = "hi"; 
- for(var name in Name2) {
-    console.log("Hello " + Name2[name])
- }
-
- console.log(Name2); 
-
-
- // js closures
-
- 
+      document.querySelector("body")
+         .addEventListener("mousemove",
+            function (event) {
+               if(event.shiftKey == true) {
+                  console.log("x: " + event.clientX);
+                  console.log("y: " + event.clientY);
+               }
+  
+            }
+         );
+   }
+);
 
 
 
 
+
+
+
+
+// console.log(document.getElementById("title"));
+
+
+
+
+//  HTML doesn't need to have our
+// event listeners
+
+
+
+// document.querySelector("button").
+// onclick = sayHello; 
